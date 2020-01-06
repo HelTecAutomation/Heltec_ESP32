c6ddf821e4ee497c38aa6983f14eda9bc869261d
Heltec-Aaron-Lee <support@heltec.cn>
2020-01-06 19:21:40 +0800

    Update Serial2 example

--- examples/ESP32/Serial2/Serial2.ino
+++ examples/ESP32/Serial2/Serial2.ino
@@ -1,26 +1,39 @@
 /*
- * Heltec Automation ESP32 Serial 2 example.
+ * Heltec Automation ESP32 Serial 1 & 2 example.
+ * shows the usage of all 3 hardware uarts
  * work with ESP32's IO MUX
+ * 
+ * to test the software and hardware
+ * wire Rx1 to Tx1 and Rx2 to Tx2
+ * type text in serial monitor
+ * text will walk trough all 2 serials
+ * result is echoed to serial (usb)
 */
 
-//HardwareSerial Serial2(2);
 
 void setup() {
-
   Serial.begin(115200);
 
+  // Serial1.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
   // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
   // The txPin & rxPin can set to any output pin
-  Serial2.begin(115200, SERIAL_8N1, 18, 17);
+  
+  Serial1.begin(115200, SERIAL_8N1, 2, 17);
+  Serial2.begin(115200, SERIAL_8N1, 22, 23);
 }
 
 void loop() {
 
   if(Serial.available()) {
     int ch = Serial.read();
-    Serial2.write(ch);  
+    Serial1.write(ch);
   }
 
+  if(Serial2.available()) {
+    int ch = Serial2.read();
+    Serial2.write(ch);
+  }
+  
   if(Serial2.available()) {
     int ch = Serial2.read();
     Serial.write(ch);
