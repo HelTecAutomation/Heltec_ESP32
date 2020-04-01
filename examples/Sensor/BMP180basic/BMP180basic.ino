54ec2bfaa76abfc39d36f59374c553e0182b4c8d
Heltec-Aaron-Lee <support@heltec.cn>
2020-04-01 14:57:27 +0800

    Add BMP180 sensor example

---
+++ examples/Sensor/BMP180basic/BMP180basic.ino
@@ -0,0 +1,89 @@
+/* Heltec Automation  BMP180 Sensors test example
+ *
+ * Function:
+ * Temperature and Pressure measurement
+ * 
+ * HelTec AutoMation, Chengdu, China
+ * www.heltec.org
+ *
+ * this project also realess in GitHub:
+ * https://github.com/HelTecAutomation/ASR650x-Arduino
+ * 
+*/
+#include "Arduino.h"
+#include "heltec.h" 
+#include <Wire.h>
+#include <BMP180.h>
+#include "string.h"
+
+BMP085 bmp;
+
+uint8_t T[20] = {"Temperature"};
+
+void setup() {
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext,LOW);
+  Serial.begin(115200);
+ 
+}
+void drawFontFaceDemo(double T,double P,double A,double R) {
+    Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
+//  Heltec.display->flipScreenVertically();
+    Heltec.display->clear();
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0   , 0  ,   "Temperature =");
+    Heltec.display->drawString(76  , 0  ,   (String)T);
+    Heltec.display->drawString(106 , 0  ,   " *C");
+    Heltec.display->drawString(0   , 16 ,   "Pressure =");
+    Heltec.display->drawString(56  , 16 ,   (String)P);
+    Heltec.display->drawString(100 , 16 ,    "Pa");
+    Heltec.display->drawString(0   , 32 ,   "Altitude   =");
+    Heltec.display->drawString(56  , 32 ,   (String)A);
+    Heltec.display->drawString(86 , 32 ,   " m");
+    Heltec.display->drawString(0   , 48 ,   "Real altitude =");
+    Heltec.display->drawString(76  , 48 ,   (String)R);
+    Heltec.display->drawString(106 , 48 ,   " m");
+    Heltec.display->display();
+}  
+void loop() {
+    if (!bmp.begin()) {
+  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  while (1) {}
+  }
+    double T =bmp.readTemperature();
+    Serial.print("Temperature = ");
+    Serial.print(T);
+    Serial.println(" *C");
+
+    double P =  bmp.readPressure();
+    Serial.print("Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    double A = bmp.readAltitude();
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+
+    double R = bmp.readAltitude(101500);
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+    Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
+    
+    Serial.println();
+    delay(500);
+	drawFontFaceDemo(T, P, A, R) ;
+	delay(5000);
+}
---
+++ src/BMP180.h
@@ -0,0 +1,58 @@
+#ifndef BMP180_H
+#define BMP180_H
+
+#include "Arduino.h"
+#include "Wire.h"
+
+#define BMP085_DEBUG 0
+
+#define BMP085_I2CADDR 0x77
+
+#define BMP085_ULTRALOWPOWER 0
+#define BMP085_STANDARD      1
+#define BMP085_HIGHRES       2
+#define BMP085_ULTRAHIGHRES  3
+#define BMP085_CAL_AC1           0xAA  // R   Calibration data (16 bits)
+#define BMP085_CAL_AC2           0xAC  // R   Calibration data (16 bits)
+#define BMP085_CAL_AC3           0xAE  // R   Calibration data (16 bits)    
+#define BMP085_CAL_AC4           0xB0  // R   Calibration data (16 bits)
+#define BMP085_CAL_AC5           0xB2  // R   Calibration data (16 bits)
+#define BMP085_CAL_AC6           0xB4  // R   Calibration data (16 bits)
+#define BMP085_CAL_B1            0xB6  // R   Calibration data (16 bits)
+#define BMP085_CAL_B2            0xB8  // R   Calibration data (16 bits)
+#define BMP085_CAL_MB            0xBA  // R   Calibration data (16 bits)
+#define BMP085_CAL_MC            0xBC  // R   Calibration data (16 bits)
+#define BMP085_CAL_MD            0xBE  // R   Calibration data (16 bits)
+
+#define BMP085_CONTROL           0xF4 
+#define BMP085_TEMPDATA          0xF6
+#define BMP085_PRESSUREDATA      0xF6
+#define BMP085_READTEMPCMD          0x2E
+#define BMP085_READPRESSURECMD            0x34
+
+
+class BMP085 {
+ public:
+  BMP085();
+  boolean begin(uint8_t mode = BMP085_ULTRAHIGHRES);  // by default go highres
+  float readTemperature(void);
+  int32_t readPressure(void);
+  int32_t readSealevelPressure(float altitude_meters = 0);
+  float readAltitude(float sealevelPressure = 101325); // std atmosphere
+  uint16_t readRawTemperature(void);
+  uint32_t readRawPressure(void);
+  
+ private:
+  int32_t computeB5(int32_t UT);
+  uint8_t read8(uint8_t addr);
+  uint16_t read16(uint8_t addr);
+  void write8(uint8_t addr, uint8_t data);
+
+  uint8_t oversampling;
+
+  int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
+  uint16_t ac4, ac5, ac6;
+};
+
+
+#endif //  _BMP085_H
